eacc623c2980bfa999e137b94165d7be7c97e34c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-09 10:42:21 +0200

    Update podman-run-ollama-nvidia.bash

--- podman-run-ollama-nvidia.bash
+++ podman-run-ollama-nvidia.bash
@@ -1,3 +1,3 @@
 mkdir -p $(pwd)/ollama_data
 
-podman run --detach --pull newer -e OLLAMA_DEBUG=1 --replace --device nvidia.com/gpu=all -e OLLAMA_API_KEY="SecureSharedSecret" -v $(pwd)/ollama_data:/root/.ollama --publish 11434:11434 --name ollama docker.io/ollama/ollama:latest
+podman run --detach --pull newer -e OLLAMA_DEBUG=1 --replace --device nvidia.com/gpu=all --env OLLAMA_API_KEY="SecureSharedSecret" --volume $(pwd)/ollama_data:/root/.ollama --publish 11434:11434 --name ollama docker.io/ollama/ollama:latest
