f99557e43e27c6f79281b652227c62f98324986e
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-09 11:17:16 +0200

    Update podman run command options

--- podman-run-open-webui.bash
+++ podman-run-open-webui.bash
@@ -1,6 +1,6 @@
 mkdir -p $(pwd)/webui_data
 
-podman run -d -p 8080:8080 \
+podman run --detach --pull newer --replace --publish 8080:8080 \
   -e OLLAMA_BASE_URL=http://host.containers.internal:11434 -e OLLAMA_TOKEN=SecureSharedSecret \
   -e WEBUI_SECRET_KEY=change-me-to-a-long-random-secret -e ENABLE_SIGNUP=false \
   -e WEBUI_ADMIN_EMAIL="admin@openwebui.local" \
